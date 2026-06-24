34fd794cf18b9622b8eefa4878fb6672b9c9aa19
SittichaiRMUTL <SittichaiRMUTL@users.noreply.github.com>
2026-06-24 21:41:46 +0700

    Add week 1 lab 7

---
+++ week1/69543206040-5_ENGCC304_lab_1_7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    char Name[30], Section[30];
+
+    scanf("%s",Name);
+    scanf("%s",Section);
+
+    printf("================\n");
+    printf("Name: %s\n",Name);
+    printf("Section: %s\n",Section);
+    printf("================\n");
+
+    return 0;
+}
